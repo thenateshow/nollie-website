cf8bfce340ece14b91bda7df1ee8d2163b8f3202
thenateshow <nathanielgreene20@gmail.com>
2024-04-04 16:39:02 -0400

    Blog page
    added blog page

--- nollie-website/src/components/Sidebar/index.js
+++ nollie-website/src/components/Sidebar/index.js
@@ -2,7 +2,7 @@ import './index.scss';
 import { Link, NavLink } from 'react-router-dom';
 import LogoN from '../../assets/images/np-logo-no-bg.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser, faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faHome, faUser, faMusic, faBlog } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
@@ -18,7 +18,10 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faUser} color="#FF9966" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
-                <FontAwesomeIcon icon={faLaptopCode} color="#FF9966" />
+                <FontAwesomeIcon icon={faMusic} color="#FF9966" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="blog-link" to="/blog">
+                <FontAwesomeIcon icon={faBlog} color="#FF9966" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#FF9966" />
